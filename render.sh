fc93a8b25e2735e332d9e083a3f1df29e9bc403d
Ekhsan Fitri <ekhsanfitri123@gmail.com>
2026-04-13 14:22:26 +0800

    Update render.sh for improved error handling
    Add error handling and default port for uvicorn.

--- render.sh
+++ render.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-pip install -r requirements.txt
-python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+set -e
+
+python -m pip install -r requirements.txt
+python -m uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
